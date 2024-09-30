c8bfc425107e8f9d166b3e3c12964b0aabbc80ad
RC_Student_lab <RC_Student_lab@10.0.0.83>
2024-09-30 20:17:48 +0000

    3rd commit

--- src/main/java/com/mycompany/poe/POE.java
+++ src/main/java/com/mycompany/poe/POE.java
@@ -14,14 +14,22 @@ public class POE {
     public static void main(String[] args) {
         
     
-        //object of scanner
-        Scanner ss= new Scanner (System.in);
-     String username;
-     String password;
-     String firstName;
-     String lastName;
-        
          
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        // Create a Login object with sample data
+        Login user = new Login("user_1", "Password1!", "John", "Doe");
+        
+        // Register the user and display the registration message
+        String registrationMessage = user.registerUser();
+        System.out.println(registrationMessage);
+        
+       
+    }
+}
         
       
    
